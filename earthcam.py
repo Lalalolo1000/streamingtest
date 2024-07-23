5c78a7647807495fa75610b703d2d1dec435574d
lalalolo1000 <etienne1000@gmx.de>
2024-07-23 15:37:49 +0200

    selenium update

--- earthcam.py
+++ earthcam.py
@@ -7,11 +7,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.service import Service as ChromeService
+
 
 chrome_options = webdriver.ChromeOptions()
+
+# Running
 chrome_options.add_argument('user-data-dir=/home/pi/.config/chromium/')
-#chrome_options.add_argument('--kiosk')
-#chrome_options.add_argument('--auto-open-devtools-for-tabs')
+
 prefs = {'exit_type': 'Normal'}
 chrome_options.add_experimental_option("prefs", {'profile': prefs})
 chrome_options.add_argument('--disable-application-cache')
@@ -19,42 +22,26 @@ chrome_options.add_argument('--disable-session-crashed-bubble')
 chrome_options.add_argument('--disable-infobars')
 chrome_options.add_argument('--disable-web-security')
 chrome_options.add_argument('--disable-application-cache')
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_experimental_option('useAutomationExtension', False)
-# driver = webdriver.Chrome('./chromedriver.exe', chrome_options =chrome_options)
+
+# Running
 driver = webdriver.Chrome(chrome_options =chrome_options)
 
+# Testing
+# service = ChromeService(executable_path='/usr/bin/chromedriver')
+# driver = webdriver.Chrome(service=service, options=chrome_options)
 
-youtube_script = """document.body.appendChild(document.querySelector('video'));
-                 document.body.style.overflowY = 'hidden';
-                 document.querySelector('video').style.width = '100%';
-                 document.querySelector('video').style.height = '100%';
-                 document.querySelector('video').style.backgroundColor = 'black';
-                 document.querySelector('ytd-app').style.display = 'none';"""
-
-earthcam_script = """document.body.appendChild(document.querySelector('video'));
-                  document.querySelector('.superWhiteWrapper').style.display = 'none';
-                  document.querySelector('video').style.backgroundColor = 'black';
-                  setTimeout(function () {ecnPlayerHTML5._playerArea._videoWindow._player._userIdleEventTimer.stop();}, 5000);"""
-
-skylinewebcams_script = """document.body.appendChild(document.querySelector('video'));
-                        if (document.querySelector('#trigCookie')) {
-                            document.querySelector('#trigCookie').style.display = 'none';
-                        }
-                        document.querySelector('.header').style.display = 'none';
-                        document.querySelector('.content').style.display = 'none';
-                        document.querySelector('video').style.backgroundColor = 'black';
-                        document.querySelector('.footer').style.display = 'none';"""
 
 i = 0
 while i < 3:
-    # link = 'https://www.skylinewebcams.com/de/webcam/italia/veneto/venezia/rialto-canal-grande.html'
-    # link = 'https://www.earthcam.com/usa/newyork/timessquare/?cam=tsrobo3'
-    # link = 'https://www.youtube.com/watch?v=K_fq3l0Mfog'
-    #link = 'https://explore.org/livecams/brown-bears/brown-bear-salmon-cam-the-riffles'
 
+    #link = "https://www.earthcam.com/usa/tennessee/nashville/?cam=nashville"
+    #link = "https://www.skylinewebcams.com/de/webcam/deutschland/north-rhine-westphalia/cologne/cologne.html"
+    #link = "https://www.whatsupcams.com/de/webcams/italien/trentino-sudtirol/muehlbach/gitschberg-jochtal-webcam-skiexpress-tal/#google_vignette"
+    
     link = '#link#'
-
     driver.set_window_position(1280, 720)
     
     wait = WebDriverWait(driver, 10)
@@ -64,37 +51,98 @@ while i < 3:
 
         driver.get(link)
 
-        if 'skylinewebcams.com' in link:
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#webcam')))
-            driver.find_element_by_css_selector('#webcam').click()
-            time.sleep(3)
-            driver.execute_script(skylinewebcams_script)
+        
+        if "earthcam.com" in link:
+            # Wait for the button to be present and clickable, then click it
+            button = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "button.fc-button.fc-cta-do-not-consent.fc-secondary-button"))
+            )
+            button.click()
 
-        if 'earthcam.com' in link:
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'video')))
-            driver.execute_script(earthcam_script)
+            # Wait for the element to be present
+            element = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "video"))
+            )
 
-        if 'youtube' in link:
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'video')))
-            driver.execute_script(youtube_script)
+            # Add multiple CSS rules to the element
+            driver.execute_script("""
+                var element = arguments[0];
+                document.body.appendChild(element);
+                element.style.cssText += 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 9999999999999999999999999;';
+
+                document.body.parentElement.style.cssText += 'min-height: 0 !important;'
+                document.body.style.cssText += 'overflow: hidden !important; height: 100vh !important;'
+            """, element)
+
+
+        if "skylinewebcams.com" in link:
+            # Wait for the button to be present and clickable, then click it
+            button = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "button.fc-button.fc-cta-consent.fc-primary-button"))
+            )
+            button.click()
+
+
+            playerbutton = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".player-poster.clickable"))
+            )
+
+            # Wait for the element to be present
+            element = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "video"))
+            )
+
+            playerbutton.click()
+
+            driver.execute_script("""
+                var element = arguments[0];
+                document.body.appendChild(element);
+                element.style.cssText += 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 9999999999999999999999999;';
+
+                document.body.style.cssText += 'overflow: hidden !important; height: 100vh !important;'
+            """, element)
+
+
+        # if "whatsupcams.com" in link:
+        #     # Wait for the button to be present and clickable, then click it
+        #     button = WebDriverWait(driver, 20).until(
+        #         EC.element_to_be_clickable((By.CSS_SELECTOR, "button.fc-button.fc-cta-consent.fc-primary-button"))
+        #     )
+        #     button.click()
+
+        #     # Wait until the iframe with the specified CSS selector is present
+        #     iframe_css_selector = "iframe.embed-responsive-item.home-page-webcam-container"
+        #     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, iframe_css_selector)))
+
+        #     # Grab the URL from the iframe
+        #     iframe_element = driver.find_element(By.CSS_SELECTOR, iframe_css_selector)
+        #     driver.switch_to.frame(iframe_element)
+
+
+        #     driver.execute_script("""
+        #         var element = document.querySelector('video');
+        #         document.body.appendChild(element);
+        #         element.style.cssText += 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 9999999999999999999999999;';
+
+        #         document.body.style.cssText += 'overflow: hidden !important; height: 100vh !important;'
+        #     """)
+
+        #     # driver.switch_to.default_content()
+
+        #     driver.execute_script("""
+        #         var element = arguments[0];
+        #         document.body.appendChild(element);
+        #         element.style.cssText += 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 9999999999999999999999999;';
+
+        #         document.body.style.cssText += 'overflow: hidden !important; height: 100vh !important;'
+        #     """, iframe_element)
 
         time.sleep(5)
 
         driver.set_window_position(0, 0)
         driver.fullscreen_window()
         
-        time.sleep(10)
-        
-        while True:
-            driver.execute_script('if (document.querySelector(".modal")) {document.querySelector(".modal").style.display = "none";}')
-            driver.execute_script('if (document.querySelector(".modal-backdrop")) {document.querySelector(".modal-backdrop").style.display = "none";}')
-            if driver.execute_script('return [...document.querySelectorAll("video")].pop().readyState < 1 || [...document.querySelectorAll("video")].pop().paused == true'):
-                print('Video stopped, waiting for restore.')
-                time.sleep(60)
-                if driver.execute_script('return [...document.querySelectorAll("video")].pop().readyState < 1 || [...document.querySelectorAll("video")].pop().paused == true'):
-                    print('Video could not be restored, reloading.')
-                    break
-            time.sleep(5)
+        time.sleep(120)
         
         print('Video not running!')
         
