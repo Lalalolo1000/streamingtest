845e318eef71c6d5b80d7af24038604e4c30ca31
lalalolo1000 <etienne1000@gmx.de>
2024-07-24 17:20:22 +0200

    update

--- serverstarter.sh
+++ serverstarter.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-sleep 10
+sleep 20
 git --git-dir=/home/pi/earth/.git --work-tree=/home/pi/earth reset --hard HEAD
 git --git-dir=/home/pi/earth/.git --work-tree=/home/pi/earth clean -n -d
 git --git-dir=/home/pi/earth/.git --work-tree=/home/pi/earth pull
-sleep 10
+sleep 20
 
 /usr/bin/node /home/pi/earth/index.js
-
-
