6b7b6aabadcf3a8dc3923923d53d53b7eb57d61f
lalalolo1000 <etienne1000@gmx.de>
2024-07-24 17:19:55 +0200

    update

--- serverstarter.sh
+++ serverstarter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 10
 git --git-dir=/home/pi/earth/.git --work-tree=/home/pi/earth reset --hard HEAD
-#git --git-dir=/home/pi/earth/.git --work-tree=/home/pi/earth clean -fd
+git --git-dir=/home/pi/earth/.git --work-tree=/home/pi/earth clean -n -d
 git --git-dir=/home/pi/earth/.git --work-tree=/home/pi/earth pull
 sleep 10
 
