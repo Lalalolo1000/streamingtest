a78a6a1ba6bc8887cf574870c4f4fd8457f88b9e
lalalolo1000 <etienne1000@gmx.de>
2024-07-24 16:39:48 +0200

    update

--- stop.sh
+++ stop.sh
@@ -9,6 +9,13 @@ formatted_id=$(printf "%02d" $id)
 # Construct the IP address
 ip_address="192.168.0.2${formatted_id}"
 
-# Execute the SSH command to kill all bash processes for the user on the target machine
-echo "stop " $ip_address
-sshpass -p raspberrypi ssh pi@$ip_address "ps aux | grep '[e]arthstarter.sh' | awk '{print $2}' | xargs kill"
+# Check if id is zero
+if [ "$id" -eq 1 ]; then
+    # If id is zero, run the local ps and kill commands
+    echo "Killing all bash processes for the user on the local machine"
+    ps aux | grep '[b]ash' | awk '{print $2}' | xargs kill
+else
+    # Otherwise, run the SSH command to kill all bash processes for the user on the target machine
+    echo "Stopping bash processes on $ip_address"
+    sshpass -p raspberrypi ssh pi@$ip_address "killall -u $USER bash"
+fi
