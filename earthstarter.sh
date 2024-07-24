db500519bbfbb342652aa88a9115e670cf2c31cd
lalalolo1000 <etienne1000@gmx.de>
2024-07-24 17:33:53 +0200

    update

--- earthstarter.sh
+++ earthstarter.sh
@@ -4,8 +4,8 @@ sleep 10
 
 for ((n=0;n<3;n++))
 do
- wget -O earthcam.py 192.168.0.201:8000/earth/$1
- python3 earthcam.py
- git --git-dir=/home/pi/earth/.git pull
+ wget -O /home/pi/earthcam.py http://192.168.0.201:8000/earth/$1
+ python3 /home/pi/earthcam.py
+ #git --git-dir=/home/pi/earth/.git pull
  sleep 10
 done
